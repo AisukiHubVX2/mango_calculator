736d6457634ad9f158123dcab3de9ecd89e8efd5
Aisuki Hub <50762464+AisukiHubVX2@users.noreply.github.com>
2024-10-13 12:34:42 +0700

    Update Trashforcoco.lua

--- Trashforcoco.lua
+++ Trashforcoco.lua
@@ -4,6 +4,4 @@ local Game = game.GameId
 
 if Game == 5938036553 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/AisukiHubVX2/Cuallines/refs/heads/main/mango.lua"))()
-else
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/AisukiHubVX2/mangotrash/refs/heads/main/mangoteen.lua"))()
 end
